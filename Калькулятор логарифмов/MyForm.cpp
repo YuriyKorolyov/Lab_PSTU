8a484ddaa69c63f71d8878b3f4dd28aba8e358c6
YuriyKorolyov <92149434+YuriyKorolyov@users.noreply.github.com>
2022-05-23 17:56:43 +0500

    Add files via upload

---
+++ Калькулятор логарифмов/MyForm.cpp
@@ -0,0 +1,13 @@
+﻿#include "MyForm.h"
+
+using namespace КалькуляторЛогарифмов;
+
+int main()
+{
+	Application::EnableVisualStyles();
+	Application::SetCompatibleTextRenderingDefault(false);
+	Application::Run(gcnew MyForm);
+
+	return 0;
+
+}
